d9b5ae30d0e74da27b14d45ed954bb4378e1a0ea
Fabian Schmid <fs@studer-raimann.ch>
2020-02-20 09:36:54 +0100

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -12,7 +12,7 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = "srfp";
-$version = "0.1.0";
+$version = "0.1.1";
 $ilias_min_version = "5.4.0";
 $ilias_max_version = "5.4.999";
 $responsible = "studer + raimann ag - Team Core 1";
